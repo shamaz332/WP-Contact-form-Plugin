9534b8a4140b81edfe6d6a187bd712d93e11af51
shamaz332 <shamazsaeed332@gmail.com>
2021-07-27 00:47:09 +0500

    setup done

---
+++ shamaz-form.php
@@ -0,0 +1,15 @@
+/**
+ * Plugin Name:       Kontact Form
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            shamaz saeed
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
