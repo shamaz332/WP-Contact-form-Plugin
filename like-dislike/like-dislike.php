0335ddb7de9b5b44d67c0ae0f55dcf065fd57f61
shamaz332 <shamazsaeed332@gmail.com>
2021-07-28 15:54:59 +0500

    setting page created

--- like-dislike/like-dislike.php
+++ like-dislike/like-dislike.php
@@ -11,6 +11,7 @@
 * Text Domain: wpaclike
 */
 
+
 //If this file is called directly, abort.
 if (!defined( 'WPINC' )) {
     die;
@@ -27,9 +28,28 @@ if ( !defined('WPAC_PLUGIN_DIR')) {
 //Include Scripts & Styles
 if( !function_exists('wpac_plugin_scripts')) {
     function wpac_plugin_scripts() {
-        wp_enqueue_style('wpac-css', WPAC_PLUGIN_DIR. 'assest/css/style.css');
-        wp_enqueue_script('wpac-js', WPAC_PLUGIN_DIR. 'assest/js/main.js', 'jQuery', '1.0.0', true );
+        wp_enqueue_style('wpac-css', WPAC_PLUGIN_DIR. 'assets/css/style.css');
+        wp_enqueue_script('wpac-js', WPAC_PLUGIN_DIR. 'assets/js/main.js', 'jQuery', '1.0.0', true );
     }
     add_action('wp_enqueue_scripts', 'wpac_plugin_scripts');
 }
 
+//Settings Page HTML
+function wpac_settings_page_html() {
+
+}
+
+//Top Level Administration Menu
+function wpac_register_menu_page() {
+
+    add_menu_page( 'Marketing Tool', 'Marketo Settings', 'manage_options', 'wpac-settings', 'wpac_settings_page_html', 'dashicons-welcome-view-site', 30 );
+}
+add_action('admin_menu', 'wpac_register_menu_page');
+
+//Sub-Level Administration Menu
+/* function wpac_register_menu_page() {
+    add_theme_page( 'WPAC Like System', 'WPAC Settings', 'manage_options', 'wpac-settings', 'wpac_settings_page_html', 30 );
+}
+add_action('admin_menu', 'wpac_register_menu_page'); */
+
+?>
